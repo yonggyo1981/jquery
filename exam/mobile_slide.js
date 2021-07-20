59a1df18e876342647d22cbf265f7d90578855ce
yonggyo1981 <yonggyo1981@gmail.com>
2021-07-20 17:11:54 +0900

    .

---
+++ exam/mobile_slide.js
@@ -0,0 +1,19 @@
+/**
+* 모바일 사이드 메뉴
+*
+*/
+const mobileSide = {
+	
+};
+
+$(function() {
+	/** 메뉴 열기 */
+	$(".side_open").click(function() {
+		
+	});
+	
+	/** 메뉴 닫기 */
+	$(".side_close").click(function() {
+		
+	});
+});
